7bd2ffd8ced5aaf3bc11a8668832c6b254f53cc6
Tracey-goode <Jerand322@gmail.com>
2025-08-03 19:06:39 -0400

    initial commit for Lab

---
+++ 308.3_lab.js
@@ -0,0 +1,21 @@
+// Fizz Buzz
+// // Part One
+
+// let x = 1
+
+// for (x = 0; x <= 100; x++) {
+//     if (x % 3 == 0 && x % 5 == 0) {
+//         console.log(`${x} FIZZ BUZZ`)
+//     } else if (x % 3 == 0) {
+//         console.log(`${x} Fizz`)
+//     } else if (x % 5 == 0) {
+//         console.log(`${x} Buzz`)
+//     } else {
+//         console.log(x)
+//     }
+// }
+
+// //
+
+// Part 3
+
